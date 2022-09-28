09ac3208aa7702ffa96ba5935295ae963f1713cf
taiwoj121 <taiwoj121@gmail.com>
2022-09-28 17:04:29 +0100

    framer motion for animation

--- src/components/Feedbacklist.jsx
+++ src/components/Feedbacklist.jsx
@@ -1,21 +1,52 @@
+import React from "react";
 import PropTypes from 'prop-types'
 import Feedbackitem from "./Feedbackitem"
+import {AnimatePresence, motion} from 'framer-motion/dist/framer-motion'
+
 
 function Feedbacklist({feedback, deleteFeedBack}) {
-    if( !feedback || feedback.length ===0 ) { return <p>There is no feedback</p>}
+    if( !feedback || feedback.length === 0 ) { return <p>There is no feedback</p>}
+
+    // return (
+    //     <div className='feedback-list'>
+           
+    //        {
+    //             feedback.map((item) => 
+    //                  (
+    //                     <Feedbackitem
+    //                     key = {item.id}
+    //                     item = {item}
+    //                     handleDelete = {deleteFeedBack}
+    //                 />
+    //                 )
+    //             )
+    //         }
+         
+    //     </div>
+    // )
+
+
     return(
         <div className="feedback-list">
-            { 
-                feedback.map((item)=> {
-                    return(
-                        <Feedbackitem
-                          key={item.id}
-                          item = {item} 
-                          handleDelete= {deleteFeedBack}
-                        />
-                    )
-                })
-            }
+            <AnimatePresence>
+            {feedback.map((item) => (
+                    <motion.div
+                    key={item.id}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    >
+                    <Feedbackitem 
+                    key={item.id}
+                    item={item}
+                    handleDelete={deleteFeedBack}
+
+                    />
+                    </motion.div>
+                )
+            )}
+            
+            </AnimatePresence>
         </div>
     )
 }
@@ -26,7 +57,7 @@ Feedbackitem.propTypes = {
         {
             id: PropTypes.number.isRequired,
             text: PropTypes.string.isRequired,
-            id: PropTypes.number.isRequired,
+            rating: PropTypes.number.isRequired,
         }
     )
   )
