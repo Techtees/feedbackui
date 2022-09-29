ca84771692409b6024fd3858a3cbd549d3af9429
taiwoj121 <taiwoj121@gmail.com>
2022-09-29 14:37:39 +0100

    moved functions to feedbackcontext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
-import { v4 as uuidv4} from 'uuid'
-import { useState } from 'react';
 import Header from "./components/Header";
 import Feedbacklist from "./components/Feedbacklist";
-import FeedBackdata from './data/FeedBackdata';
 import Feedbackstats from './components/Feedbackstats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from "./pages/AboutPage";
@@ -13,18 +10,6 @@ import FeedBackContext, { FeedBackProvider } from "./context/FeedBackContext";
 
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedBackdata)
-
-  const handleDelete = (id) => {
-   if(window.confirm('are sure you want to delete')){
-     setFeedback(feedback.filter((item) => item.id !== id))
-   }
-  }
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback])
-  }
-
   return (
     <FeedBackProvider>
       <Router>
@@ -33,12 +18,9 @@ function App() {
           <Routes>
             <Route exact element = {
               <>
-              <FeedbackForm handleAdd={addFeedback} />
-              <Feedbackstats/>
-              <Feedbacklist 
-                
-                deleteFeedBack = {handleDelete}
-                />
+              <FeedbackForm />
+              <Feedbackstats />
+              <Feedbacklist />
               </>
             } path="/" />
             
