8af299a97e0bdd3c41cd12eb78684f84286fab34
taiwoj121 <taiwoj121@gmail.com>
2022-09-29 14:41:26 +0100

    replaced props with context  edit icon added

--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { FaTimes} from 'react-icons/fa'
+import { FaTimes, FaEdit} from 'react-icons/fa'
 import PropTypes from 'prop-types'
+import { useContext } from "react";
+import FeedBackContext from "../context/FeedBackContext";
 
 import Card from "./shared/Card";
 
-function Feedbackitem({item,handleDelete}){
+function Feedbackitem({item}){
+    const {deleteFeedback,feedBackEdit} = useContext(FeedBackContext)
     return(
         <Card>
             <div className="num-display">{item.rating}</div>
             <div className="text-display">{item.text}</div>
-            <button className='close' onClick ={() => handleDelete(item.id) }>
+            <button className='close' onClick ={() => deleteFeedback(item.id) }>
                  <FaTimes color='purple' />
+            </button>
+            <button className='edit' onClick={ () => feedBackEdit(item)}>
+                 <FaEdit color='purple' />
             </button>
         </Card>
     )
